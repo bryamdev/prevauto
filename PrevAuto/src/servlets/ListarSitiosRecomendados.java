d6e1670656a25b7b023ec7eb346ccff2f0df89fb
Christian Ramirez <christianram19@hotmail.com>
2019-07-24 18:32:16 -0500

    CAMBIO A LISTAR LOS SITIOS

--- PrevAuto/src/servlets/ListarSitiosRecomendados.java
+++ PrevAuto/src/servlets/ListarSitiosRecomendados.java
@@ -8,36 +8,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.*;
-/**
- * Servlet implementation class ListarSitiosRecomendados
- */
+/
 @WebServlet("/ListarSitiosRecomendados")
 public class ListarSitiosRecomendados extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ListarSitiosRecomendados() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		PrintWriter out = response.getWriter();
 		out.println("Hola perras");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
+	
 }
