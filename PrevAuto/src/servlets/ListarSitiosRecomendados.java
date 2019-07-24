7b095961ec721303bf3cbdbbd110e03cdd3126ec
Christian Ramirez <christianram19@hotmail.com>
2019-07-24 18:48:50 -0500

    Configurando eclipse

--- PrevAuto/src/servlets/ListarSitiosRecomendados.java
+++ PrevAuto/src/servlets/ListarSitiosRecomendados.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.*;
-/
+
 @WebServlet("/ListarSitiosRecomendados")
 public class ListarSitiosRecomendados extends HttpServlet {
 	
