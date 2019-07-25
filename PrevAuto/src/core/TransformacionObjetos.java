c196784d5bd6034f711dee4e3b6a557e793ac5f0
Christian Ramirez <christianram19@hotmail.com>
2019-07-24 22:57:21 -0500

    aaaa

--- PrevAuto/src/core/TransformacionObjetos.java
+++ PrevAuto/src/core/TransformacionObjetos.java
@@ -2,21 +2,33 @@ package core;
 
 import java.util.List;
 import com.google.gson.Gson;
-import datos.SitiosJDBC;
-import domain.Sitio;
+import datos.*;
+import domain.*;
 
 public class TransformacionObjetos {
 	
-	SitiosJDBC sitioJDBC = new SitiosJDBC();
+	Gson gson = new Gson();
+	SitiosJDBC sitioJDBC = null;
+	VehiculosJDBC vehiculoJDBC = null;
+	
 	//Toma la lista de Sitios y lo serializa a json
 	//Devuelve un string con el json de los sitios
 	public String obtenerJsonSitios() {
 	
+		sitioJDBC = new SitiosJDBC();
 		List<Sitio> sitios = sitioJDBC.selectSitios();
-		Gson gson = new Gson();
 		String sitiosJson = gson.toJson(sitios);
 	
 		return sitiosJson;
+	}
+	
+	
+	public String obtenerVehiculosJson(int idUsuario) {
+		vehiculoJDBC = new VehiculosJDBC();
+		List<Vehiculo> vehiculos = vehiculoJDBC.selectVehiculos(idUsuario);
+		String vehiculosJson = gson.toJson(vehiculos);
+		
+		return vehiculosJson;
 	}
 			
 }
