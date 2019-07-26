628a1032efeca27eef6fdc2770d2ca4def84f643
Christian Steven Ramirez Devia <52114316+ChristianRam@users.noreply.github.com>
2019-07-25 21:43:20 -0500

    Add files via upload

---
+++ PrevAuto/src/domain/VerDetalles.java
@@ -0,0 +1,24 @@
+package domain;
+
+public class VerDetalles {
+	
+	private int idVehiculo;
+	private String nombre;
+	private String modelo;
+	private String marca;
+	private String placa;
+	
+	
+	public VerDetalles(int idVehiculo, String nombre, String modelo, String marca, 
+			String placa) {
+		
+		this.idVehiculo = idVehiculo;
+		this.nombre = nombre;
+		this.modelo = modelo;
+		this.marca = marca;
+		this.placa = placa;
+	}
+	
+	
+	
+}
