224b099f2f3bf1bd88be86f96f07b52785040fc0
Christian Ramirez <christianram19@hotmail.com>
2019-07-27 10:39:13 -0500

    Servicio de listado de documentos

--- PrevAuto/src/servicios/ListarDocumentos.java
+++ PrevAuto/src/servicios/ListarDocumentos.java
@@ -1,11 +1,15 @@
 package servicios;
 
 import java.io.*;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
+import core.TransformacionObjetos;
 import datos.DocumentosJDBC;
+import domain.Documento;
 
 
 @WebServlet("/ListarDocumentos")
@@ -13,11 +17,17 @@ public class ListarDocumentos extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
-		int idVehiculoReq = 1; 
+		int idVehiculoReq = Integer.parseInt(request.getParameter("idVehiculo")); 
 		
-		//List<Documento> listaDocumentos = DocumentosJDBC
+		List<Documento> listaDocumentos = DocumentosJDBC.selectDocumentos(idVehiculoReq);
 		
+		String json = TransformacionObjetos.obtenerJson(listaDocumentos);
 		
+		response.setContentType("application/json");
+		
+		PrintWriter out = response.getWriter();
+		out.print(json);
+		out.flush();
 	}
 
 }
