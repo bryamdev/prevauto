139a153d0bbeec16e50807c69e8948f296dd2059
Christian Steven Ramirez Devia <52114316+ChristianRam@users.noreply.github.com>
2019-07-27 12:59:31 -0500

    Add files via upload

---
+++ PrevAuto/src/servicios/VerAlertas.java
@@ -0,0 +1,31 @@
+package servicios;
+
+import java.io.*;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+import core.TransformacionObjetos;
+import datos.VerAlertasJDBC;
+import domain.Alerta;
+
+@WebServlet("/VerAlertas")
+public class VerAlertas extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		int idUsuarioReq = Integer.parseInt(request.getParameter("idUsuario"));
+		
+		List<Alerta> listaAlertas = VerAlertasJDBC.selectAlertas(idUsuarioReq);
+		String json = TransformacionObjetos.obtenerJson(listaAlertas);
+		
+		response.setContentType("application/json");
+		PrintWriter out = response.getWriter();
+		
+		out.print(json);
+		out.flush();
+	}
+
+}
